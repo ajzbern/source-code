7425fd8a001111f99bf15dd8f8c8478ffd9f53d3
Gajjar Mohit <114332766+Gajjar-Mohit@users.noreply.github.com>
2025-10-03 10:54:33 +0530

    Update interface.py

--- agent-service/agents/models/openai/interface.py
+++ agent-service/agents/models/openai/interface.py
@@ -30,17 +30,3 @@ def init_openai(model):
     return llm
 
 
-
-# from langchain_nvidia_ai_endpoints import ChatNVIDIA
-
-# client = ChatNVIDIA(
-#   model="deepseek-ai/deepseek-r1",
-#   api_key="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", 
-#   temperature=0.6,
-#   top_p=0.7,
-#   max_tokens=4096,
-# )
-
-# response = client.invoke([{"role":"user","content":""}])
-# print(response.content)
-
